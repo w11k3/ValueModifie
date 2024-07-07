8a9a052dd9a786344f11a634ad6e82086a159f42
Paulo Kinjo <paulokinjo@hotmail.com>
2024-07-06 20:57:15 -0400

    Making value modifie

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿public class PassArray
+{
+    // Main creates array and calls ModifyArray and ModifyElement
+    public static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Effects of passing reference to entire array:\n" + "The values of the original array are:");
+
+        // output original array elements
+        foreach (int value in array)
+            Console.Write($" {value}");
+
+        ModifyArray(array); // pass array reference
+        Console.WriteLine("\n\nThe values of the modified array are:");
+
+        // output modified array elements
+        foreach (int value in array)
+            Console.Write($" {value}");
+
+        Console.WriteLine("\n\nEffects of passing array element value:\n" +
+            $"array[3] before ModifyElement: {array[3]}");
+
+        ModifyElement(array[3]); // attempt to modify array[3]
+        Console.WriteLine($"array[3] after ModifyElement: {array[3]}");
+    } // end Main
+
+    // multiply each element of an array by 2
+    public static void ModifyArray(int[] array2)
+    {
+        for (int counter = 0; counter < array2.Length; ++counter)
+            array2[counter] *= 2;
+    } // end method ModifyArray
+
+    // multiply argument by 2
+    public static void ModifyElement(int element)
+    {
+        element *= 2;
+        Console.WriteLine($"Value of element in ModifyElement: {element}");
+    } // end method ModifyElement
+} // end class PassArray
